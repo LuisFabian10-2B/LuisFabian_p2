7a6f67747615b738b6fe4472c2409970e088becb
LuisFabian2 <89663840+LuisFabian10-2B@users.noreply.github.com>
2021-10-13 17:02:17 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+int num_1=0;
+int num_2=0;
+int result_1=0;
+cout<<"Suma\n";//operacion de suma/
+cout<<"digite un numero\n: ";//indicacion/
+cin >>num_1; //primer numero/
+cout<<"digite otro numero\n: ";//indicacion/
+cin >>num_2;//segundo numero/
+result_1=num_1+num_2;
+cout<<result_1;//resultado/
+}
